fe7f79c88393402a2aae0a55553eb5c7804b5393
techGirl0 <254766232+techGirl0@users.noreply.github.com>
2026-01-28 16:55:26 +0100

    Register Input: Add function to read player move and return coordinates
    Register Input: Create function and basic input prompt

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ board = [
 
 player1 = {'turn': -1, 'symbol': ''}
 player2 = {'turn': -1, 'symbol': ''}
+
+# register user input
+def registerInput(currentPlayer, board):
+    move = int(input(f"{currentPlayer['symbol']}'s turn. Choose a position (1-9): "))
+    print("You entered:", move)
